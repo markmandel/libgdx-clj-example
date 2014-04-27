065782a99675410cd31005ed9260ed07de01db49
Mark Mandel <mark.mandel@gmail.com>
2014-04-27 17:40:57 +1000

    Initial creation

---
+++ README.md
@@ -0,0 +1,18 @@
+# libgdx-clj-example
+
+Example clojure application with native libgdx integration.
+
+Also a bit of a testbed for libgdx concepts.
+
+## Usage
+
+```
+lein run
+```
+
+## License
+
+Copyright © 2014 Mark Mandel
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject libgdx-clj-example "0.1.0-SNAPSHOT"
+    :description "Example clojure application with native libgdx integration."
+    :url "http://github.com/markmandel/libgdx-clj-example"
+    :license {:name "Eclipse Public License"
+              :url  "http://www.eclipse.org/legal/epl-v10.html"}
+    :dependencies [[com.badlogicgames.gdx/gdx "1.0.0"]
+                   [com.badlogicgames.gdx/gdx-backend-lwjgl "1.0.0"]
+                   [com.badlogicgames.gdx/gdx-box2d "1.0.0"]
+                   [com.badlogicgames.gdx/gdx-box2d-platform "1.0.0"
+                    :classifier "natives-desktop"]
+                   [com.badlogicgames.gdx/gdx-platform "1.0.0"
+                    :classifier "natives-desktop"]
+                   [org.clojure/clojure "1.6.0"]]
+
+    :aot :all
+    :main libgdx-clj-example.core
+    :profiles {})
