9cafb085139e189eb20b3b5200e22be6164b8c38
Mark Mandel <mark.mandel@gmail.com>
2014-04-29 04:49:03 +1000

    I have a sqaure, but it's aspect ratio is skewiff.

--- src/libgdx_clj_example/game.clj
+++ src/libgdx_clj_example/game.clj
@@ -1,6 +1,7 @@
 (ns libgdx-clj-example.game
     (:import [com.badlogic.gdx Game Screen ScreenAdapter Gdx]
-             (com.badlogic.gdx.graphics GL20)))
+             (com.badlogic.gdx.graphics GL20 OrthographicCamera Color)
+             (com.badlogic.gdx.graphics.glutils ShapeRenderer ShapeRenderer$ShapeType)))
 
 (gen-class :name example.core.Game
            :extends com.badlogic.gdx.Game
@@ -8,10 +9,18 @@
 
 ;; cheap and easy way to change methods on reload. Delegate to another method!
 
-(defn- render [delta]
+(defn- render [cam shapes delta]
     (doto Gdx/gl
         (.glClearColor 0 0 0 0)
-        (.glClear GL20/GL_COLOR_BUFFER_BIT)))
+        (.glClear GL20/GL_COLOR_BUFFER_BIT))
+    (doto cam
+        (.update))
+    (doto shapes
+        (.setProjectionMatrix (.combined cam))
+        (.begin ShapeRenderer$ShapeType/Filled)
+        (.setColor Color/WHITE)
+        (.rect 50 50 50 50)
+        (.end)))
 
 (defn- resize [w h]
     (println "Resize! " w " x " h))
@@ -19,16 +28,19 @@
 (defn create-screen
     "Make a screen"
     []
-    (proxy [Screen] []
-        (show [])
-        (render [delta]
-            (render delta))
-        (dispose [])
-        (hide [])
-        (pause [])
-        (resize [w h]
-            (resize w h))
-        (resume [])))
+    (let [cam (OrthographicCamera. 600 600)
+          shapes (ShapeRenderer.)]
+        (proxy [Screen] []
+            (show []
+                (.set (.position cam) 300 300 0))
+            (render [delta]
+                (render cam shapes delta))
+            (dispose [])
+            (hide [])
+            (pause [])
+            (resize [w h]
+                (resize w h))
+            (resume []))))
 
 ;; Game class methods
 
